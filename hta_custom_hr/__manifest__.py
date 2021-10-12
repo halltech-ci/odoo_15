e7c233b2ce34ff15c4fe1b66f5160dcae2eda5c8
ATCHE Maurice <maurice.atche@halltech-africa.com>
2021-10-12 21:52:21 +0000

    Adding account info in contract view

--- hta_custom_hr/__manifest__.py
+++ hta_custom_hr/__manifest__.py
@@ -16,20 +16,25 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Human Resources/Payroll',
+    'version': '15.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['hr_contract',
+               'account',
+               ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        #views,
+        'views/hr_contract_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'license': 'LGPL-3',
 }
