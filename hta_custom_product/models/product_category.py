92c64f0a493d5e59c78d3d8ef69a318765e2e274
ATCHE Maurice <maurice.atche@halltech-africa.com>
2021-11-01 10:54:14 +0000

    Removing store=True in related_code

--- hta_custom_product/models/product_category.py
+++ hta_custom_product/models/product_category.py
@@ -15,18 +15,22 @@ class ProductCategory(models.Model):
     """    
     
     category_code = fields.Char()
-    related_code = fields.Char(string='Related Code', compute = '_compute_related_code', recursive=True)
+    related_code = fields.Char(string='Related Code', compute = '_compute_related_code', recursive=True,)
     
     @api.depends('parent_id.related_code', 'category_code')
     def _compute_related_code(self):
-        for categ in self:
-            if categ.parent_id:
-                categ.related_code = str(self.parent_id.related_code) + '-' + str(self.category_code)
+        for category in self:
+            if category.parent_id:
+                category.related_code = '%s-%s' % (category.parent_id.related_code, category.category_code)
             else:
-                categ.related_code = str(self.category_code)
+                category.related_code = str(category.category_code)
     
     @api.onchange('parent_id')
     def onchange_parent_id(self):
-        self.related_code = str(self.parent_id.related_code) + '-' + str(self.category_code) 
+        for category in self:
+            category.related_code = '%s-%s' % (category.parent_id.related_code, category.category_code)
         
-    
+    @api.onchange('category_code')
+    def onchange_category_code(self):
+        for category in self:
+            category.related_code = '%s-%s' % (category.parent_id.related_code, category.category_code)
