19f13cd418da7dd691d9aa3e294d5b29f21a4aab
ATCHE Maurice <maurice.atche@halltech-africa.com>
2021-11-03 01:34:32 +0000

    Deleting post_init_hooks

--- hta_custom_product/hooks.py
+++ hta_custom_product/hooks.py
@@ -11,20 +11,3 @@ def pre_init_product_category_code(cr):
     cr.execute("ALTER TABLE product_category " "ADD COLUMN category_code character varying;")
     cr.execute("UPDATE product_category " "SET category_code = id;")
     
-"""
-def post_init_product_category_code(cr, registry):
-    """
-    This post-init-hook will update all existing task assigning them the
-    corresponding sequence code.
-    """
-    env = api.Environment(cr, SUPERUSER_ID, dict())
-    category_obj = env["product.category"]
-    sequence_obj = env["ir.sequence"]
-    categories = env["product.category"].search([], order="id")
-    for category_id in categories.ids:
-        cr.execute(
-            "UPDATE product_category " "SET category_code = %s " "WHERE id = %s;",
-            (sequence_obj.next_by_code("product.category.code"), category_id),
-        )
-"""
-        
