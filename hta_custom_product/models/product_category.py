03888cd86e6b30c8987cf8e7210f849a9704819b
ATCHE Maurice <maurice.atche@halltech-africa.com>
2021-11-03 00:53:21 +0000

    Adding hooks and  unique constraint

--- hta_custom_product/models/product_category.py
+++ hta_custom_product/models/product_category.py
@@ -9,7 +9,7 @@ class ProductCategory(models.Model):
     def _get_default_category_code(self):
         return self.env["ir.sequence"].next_by_code("product.category.code")
     
-    category_code = fields.Char(index=True, default=get_default_category_code)
+    category_code = fields.Char(index=True, default=_get_default_category_code)
     related_code = fields.Char(string='Related Code', compute = '_compute_related_code', recursive=True, store=True, search='_search_related_field',)
     
     @api.depends('parent_id.related_code', 'category_code')
